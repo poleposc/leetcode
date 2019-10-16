8f28d7cc32f41ceacfd6986c0bd5fad147e7153b
black <mogudream@163.com>
2019-10-16 19:11:44 +0800

    init

---
+++ 1.两数之和.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target - num] = i
+        
+# @lc code=end
+
